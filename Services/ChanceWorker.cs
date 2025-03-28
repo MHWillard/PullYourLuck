99961d015efd87063403cf59e2b71c71f61c3d36
MHWillard <mhwillard@gmail.com>
2025-03-28 16:02:56 -0400

    initial

---
+++ Services/ChanceWorker.cs
@@ -0,0 +1,27 @@
+﻿namespace PullYourLuck.Services
+{
+    public class ChanceWorker
+    {
+        public bool RollChances(int SuccessChance, int BustChance)
+        {
+            if (SuccessChance > BustChance)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int ModifySuccess(int SuccessChance, int Interval) 
+        { 
+            int NewSuccess = SuccessChance - Interval;
+            return NewSuccess;
+        }
+
+        public int ModifyBust(int BustChance, int Interval)
+        {
+            int NewBust = BustChance + Interval;
+            return NewBust;
+        }
+    }
+}
